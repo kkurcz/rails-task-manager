8ab73fc54a103c77ddc88bc12d032092e7ca2c76
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-03-30 11:56:50 +0800

    added task model and seeded databse with 'faker' data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+100.times do
+  t = Task.create!(
+    title: Faker::JapaneseMedia::DragonBall.character,
+    details: Faker::JapaneseMedia::DragonBall.planet,
+    completed: false
+    )
+  puts "Task #{t.title} with details #{t.details} is created, complete: #{t.completed}"
+end
+
+
